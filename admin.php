2eced4ccc83bafb77c656709e1008ec5f5df0439
maxdosme <233705835@qq.com>
2017-12-07 10:00:12 +0800

    提交后台数据库提取逻辑

---
+++ admin.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="utf-8">
+    <title></title>
+</head>
+<body>
+<section class="container">
+        <?php
+        //设置服务器的连接信息
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "testdb";
+
+        // 创建连接
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // 检测连接
+        if ($conn->connect_error) {
+            die("连接失败: " . $conn->connect_error);
+        }
+        // 设置字符集utf-8
+        if (!$conn->set_charset("utf8")) {
+            printf("Error: %s\n", $conn->error);
+        } else {}
+
+       $sql = "SELECT id,name,sex,phone FROM user";
+        $result = $conn->query($sql);
+        if ($result) {
+            if($result->num_rows>0){                                         //判断结果集中行的数目是否大于0
+                while($row =$result->fetch_array() ){                        //循环输出结果集中的记录
+                    echo <<< EOT
+                        <div class="user_boxAll">
+                            <div class="user_box">
+                                <ul>
+                                    <li class="">ID号:$row[0]</li>
+                                    <li>客户名：$row[1]</li>
+                                    <li>性别：$row[2]</li>
+                                    <li>联系电话：$row[3]</li>
+                                    <hr />
+                                </ul>
+                            </div>
+                        </div>
+EOT;
+
+                    /*echo "<div class='user_id'>ID号：".($row[0])."</div>";
+                    echo "<div class='user_name'>客户名：".($row[1])."</div>";
+                    echo "<div class='user_sex'>性别：".($row[2])."</div>";
+                    echo "<div class='user_phone'>联系电话：".($row[3])."</div>";
+                    echo "<hr>";*/
+                }
+            }
+        }else {
+            echo "查询失败";
+        }
+        $conn->close();
+
+        ?>
+    <a href="./">网站首页</a>
+</section>
+<script src="js/jquery-1.8.3.min.js"></script>
+<script>
+    $(document).ready(function () {
+        $(".user_box").each(function () {
+            $(this).prependTo(".container");
+        });
+    })
+</script>
+</body>
+</html>
--- js/main.js
+++ js/main.js
@@ -2,4 +2,30 @@
  * Created by BenTfu on 2017/11/24.
  */
 
-
+$(document).ready(function () {
+    $('#submit').click(function () {
+        var username = $('#username').val();
+        var phone = $('#phone').val();
+        var sex = $('input:radio[name = "sex1"]:checked').val();
+        if (username.length == "") {
+            alert("用户名不能为空！");
+            $('#username').focus();
+        }else if(phone.length == "") {
+            alert('手机号不能为空！');
+            $('#phone').focus();
+        }else if (!phone.match(/^(13[0-9]|14[0-9]|15[0-9]|18[0-9])\d{8}$/)) {
+            alert("手机号码格式不正确！");
+            $("#phone").focus();
+        }else {
+            $.post('userinfo_insert.php',{xingming:username,myphone:phone,sex1:sex},function(data) {
+                if(data=='registerSuccess'){
+                    alert('恭喜您,注册成功！');
+                }else{
+                    $("#username").val("");
+                    $("#phone").val("");
+                    alert('提交成功！稍后会有专业老师联系您！');
+                }
+            });
+        }
+    })
+});
